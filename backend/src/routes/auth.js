b5f3fe549a29995877ad1da1eb45db60f44f0b45
Ayush Trikmani <143244012+AyushTrikmani@users.noreply.github.com>
2026-05-02 12:27:38 +0530

    Delete backend directory

--- backend/src/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const { register, login, getMe } = require('../controllers/authController');
-const { auth } = require('../middleware/auth');
-
-router.post('/register', register);
-router.post('/login', login);
-router.get('/me', auth, getMe);
-
-module.exports = router;
